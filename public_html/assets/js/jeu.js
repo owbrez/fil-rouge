d2d1b2d47bc084e883ad453f6d5c2551d583151d
GMB.CAPELLE <gmb.capelle@gmail.com>
2018-10-12 12:25:01 +0200

    Accordéon de cartes + son sur page connexion

--- public_html/assets/js/jeu.js
+++ public_html/assets/js/jeu.js
@@ -1,20 +1,57 @@
+var lesCartes = [];
+var lesCartesSelectionees = [];
 $(document).ready(function(){
     var nbCartes = $('.zoneBas .uneCarte').length;
-    var lesCartes = [];
     $('.zoneBas .uneCarte').each(function(){
         lesCartes.push(this);
     });
     orienterCartes(lesCartes,nbCartes);
+    $('.zoneBas .uneCarte:not(.selectionee)').hover(mettreEnAvantCarte,positionInitialeCarte);
+    $('.zoneBas .uneCarte').on('click',selectionCarte);
+    $('.zoneBas .uneCarte').mouseenter(mettreEnAvantCarte);
+    $('.zoneBas .uneCarte:not(.selectionee)').mouseleave(positionInitialeCarte);
 });
 
 function orienterCartes(lesCartes,nbCartes){
-    var compteur = 180/(nbCartes + 2);
-    var angle = -90 + compteur;
-    var translateX = 0;
+    var demiCercle = 0;
+    var degree = 60; 
+    var topInit = -80;
+    var leftInit = 0;
     for(var i = 0; i< nbCartes; i++){
-        $(lesCartes[i]).css({"transform":"rotate(" + angle + "deg"});
-        console.log(angle);
-        angle += compteur;
-        angle += translateX;
+
+        degRotate = degree-100;
+        $(lesCartes[i]).css("transform","rotate("+degRotate+"deg)");
+        $(lesCartes[i]).css("-moz-transform","rotate("+degRotate+"deg)");
+        $(lesCartes[i]).css("-webkit-transform","rotate("+degRotate+"deg)");
+        $(lesCartes[i]).css("-o-transform","rotate("+degRotate+"deg)");
+        rad  = degree/57.2;
+        posX = Math.round(leftInit-(Math.cos(rad)*600));
+        posY = Math.round(topInit+(Math.sin(rad)*200));
+        $(lesCartes[i]).css("left",posX+"px");
+        $(lesCartes[i]).css("bottom",posY+"px");
+        degree = degree + Math.round(degree/nbCartes);
+    }
+}
+
+function mettreEnAvantCarte(){
+    $(this).css('z-index','30');
+}
+
+function positionInitialeCarte(){
+    $(this).css('z-index','0');
+}
+
+function selectionCarte()
+{
+    if($('.zoneBas .uneCarte').length < 2){
+    if($(this).hasClass('selectionee')){
+        $(this).removeClass('selectionee');
+    }else{
+        $(this).addClass('selectionee'); 
+        lesCartesSelectionees.push($(this)); 
+    }
+    }else{
+        $(lesCartesSelectionees[0]).removeClass('selectionee');
+        
     }
 }
--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -2,8 +2,26 @@
 $(document).ready(function(){
     
    $('label.avatar').click (ajouterClasse('connexion label.avatar','checked'));
+    $('label.labelAvatarUn').click(function(){playSonCourt('assets/son/avatars/lezard.mp3');});
+    $('label.labelAvatarDeux').click(function(){playSonCourt('assets/son/avatars/druide.mp3')});
+    $('label.labelAvatarTrois').click(function(){playSonCourt('assets/son/avatars/paladin.mp3')});
+    $('label.labelAvatarQuatre').click(function(){playSonCourt('assets/son/avatars/magicien.mp3')});
+    playMusiquefond();
 });
 
 function ajouterClasse(label,classe){
     $(label).addClass(classe);
+}
+
+function playSonCourt(url){
+    var audio = new Audio(url);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function playMusiquefond(){
+    var audio = new Audio('assets/son/fond/musique-fond.mp3');
+    audio.volume = 0.2;
+    audio.play();
+    
 }
